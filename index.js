879586d15268074edd0dbe569d1a56e36c6f4bd9
githubscientist <sathish.datascientist@gmail.com>
2026-03-07 11:20:43 +0530

    getElementsByTagName --method explained

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello world!');
+// window -> refers to the browser's window.
+// window.location.href = 'https://guvi.in';
+
+// console.log(window.document); // refers to the html document
+
+// read or access html elements in javascript
+
+let listItems = window.document.getElementsByTagName('ul')[0].children;
+
+// for (let i = 0; i < listItems.length; i++) {
+//     console.log(listItems[i].textContent);
+// }
+
+for (let i = 0; i < listItems.length; i++) {
+    listItems[i].textContent = 'Coffee';
+}
