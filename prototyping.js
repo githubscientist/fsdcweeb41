b1c72c475663d2ab5b047d48a7f7db1b4bf600d1
githubscientist <sathish.datascientist@gmail.com>
2026-03-21 10:59:07 +0530

    media queries

--- prototyping.js
+++ prototyping.js
@@ -1,25 +1,16 @@
-/* 
+function DOB(date = 1, month = 'Jan', year = 1970) {
+    this.date = date;
+    this.month = month;
+    this.year = year;
 
-    After ES6, class syntax.
-
-*/
-
-class DOB {
-
-    constructor(date = 1, month = 'Jan', year = 1970) {
-        this.date = date;
-        this.month = month;
-        this.year = year;
-    }
-
-    print() {
+    this.print = () => {
         console.log(`Date: ${this.date}`);
         console.log(`Month: ${this.month}`);
         console.log(`Year: ${this.year}`);
         console.log();
     }
 
-    calcAge = () => {
+    this.calcAge = () => {
         console.log(new Date().getFullYear() - this.year);
     }
 }
@@ -27,7 +18,7 @@ class DOB {
 const dob_101 = new DOB(14, 'Mar', 2000);
 const dob_102 = new DOB(10, 'Feb', 1980);
 
-dob_101.print();
+// dob_101.print();
 // dob_102.print();
 
-// dob_102.calcAge();
+dob_102.calcAge();
