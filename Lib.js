5a55db9202c9ec5ba3ea75258db33c6c7f5dc4be
githubscientist <sathish.datascientist@gmail.com>
2026-03-21 13:17:33 +0530

    js types explaine

--- Lib.js
+++ Lib.js
@@ -2,4 +2,11 @@ function add(x, y) {
     console.log(`the sum of ${x} and ${y} is ${x + y}`);
 }
 
-export default add;
+function sub(x, y) {
+    console.log(`the difference of ${x} and ${y} is ${x - y}`);
+}
+
+module.exports = {
+    add,
+    sub
+}
--- Main.js
+++ Main.js
@@ -1,3 +1,4 @@
-import add from "./Lib.js";
+const { add, sub } = require('./Lib');
 
 add(5, 6);
+sub(6, 10);
