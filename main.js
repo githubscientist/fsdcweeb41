67167a9c1baa0047550a2066ee5886fa77889067
githubscientist <sathish.datascientist@gmail.com>
2026-03-21 12:02:10 +0530

    assigning a different datatype value

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 var x = 5;
+x = 'ten';
 console.log('the value of x is', x);
 console.log('data type of x is', typeof x);
--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-let x:number = 5;
+let x: number = 5;
+
+x = 'ten';
 
 console.log('the value of x is', x);
 console.log('data type of x is', typeof x);
