1244b65554348b9b992f09f2b7fa27c3c4460740
githubscientist <sathish.datascientist@gmail.com>
2026-04-09 16:27:36 +0530

    typescript fundamentals

--- dist/main.js
+++ dist/main.js
@@ -1,29 +1,11 @@
 // let name: string = 'sathish';
-// let age: number = 35;
-// let isStudent: boolean = true;
-// let marks: number[] = [70, 80, 90, 100];
-// let student: {
-//     name: string,
-//     age: number
-// } = {
-//     name: "sathish",
-//     age: 35
-// };
-// console.log(student);
-// let person: [string, number] = ["sathish", 35];
-// person.push(56);
-// person[0] = "John";
-// console.log(person);
-var Day;
-(function (Day) {
-    Day[Day["Sunday"] = 0] = "Sunday";
-    Day[Day["Monday"] = 1] = "Monday";
-    Day[Day["Tuesday"] = 2] = "Tuesday";
-    Day[Day["Wednesday"] = 3] = "Wednesday";
-    Day[Day["Thursday"] = 4] = "Thursday";
-    Day[Day["Friday"] = 5] = "Friday";
-    Day[Day["Saturday"] = 6] = "Saturday";
-})(Day || (Day = {}));
-console.log(Day.Wednesday);
+// using function prototyping
+function Student(name, age, course) {
+    this.name = name;
+    this.age = age;
+    this.course = course;
+}
+let student101 = new Student('sathish', 35, 'FSD');
+console.log(student101);
 export {};
 //# sourceMappingURL=main.js.map
