4eb1c666d82cf0f16e06ea835c666d4b680a492d
githubscientist <sathish.datascientist@gmail.com>
2026-04-11 11:50:29 +0530

    function prototypig

--- Object.js
+++ Object.js
@@ -1,70 +1,26 @@
-// template or blue print or model object
-class Student {
-    // method -> a function specific to the class Student
-    // this method will have all the variables
-    // constructor gets called whenever we create an object to this
-    // class Student
-    constructor(id = 0, name = '', place = '', mobile = '') {
-        this.id = id;
-        this.name = name;
-        this.place = place;
-        this.mobile = mobile;
-    }
+// Before2015, ES6 => EcmaScript 6
+function Student(id = 0, name = '', place = '', mobile = '') {
+    this.id = id;
+    this.name = name;
+    this.place = place;
+    this.mobile = mobile;
 
-    setID(id = 0) {
-        this.id = id;
-    }
-
-    set _name(value) {
-        value = value.charAt(0).toUpperCase() + value.slice(1,);
-        this.name = value;
-    }
-
-    print() {
+    this.print = () => {
         console.log(`ID: ${this.id}, Name: ${this.name}, Place: ${this.place}, Mobile: ${this.mobile}`);
     }
 }
 
 let students = [];
-// constructor gets called when we create an object
-// it creates a copy of the existing blue print or template
-// student1 -> refers to the reference of the copy of class Student
 students.push(new Student(47, 'sathish', 'India', '9080706050'));
-students.push(new Student(48, 'krish', 'United States', '6050403020'));
-students.push(new Student(49, 'suresh', 'United Kingdom', '4030201000'));
-
-// 48, 'krish', 'United States', '6050403020'
-
-// student3.id = 49;
-// student3.name = 'suresh';
-// student3.place = 'United Kingdom';
-// student3.mobile = '4030201000';
-
-// student2.setID(48);
-// student2._name = 'krish';
-
-// student1.print();
-// student2.print();
-// // console.log(student3);
-// student3.print();
 
 students.forEach(student => {
     student.print();
+    // console.log(student);
 })
 
-
-/*
-    student1 = {
-        id: 0, 
-        name: '', 
-        place: '', 
-        mobile: ''
+class ListNode {
+    constructor(val, next) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
     }
-    
-    student2 = {
-        id: 0, 
-        name: '', 
-        place: '', 
-        mobile: ''
-    }
-*/
+}
